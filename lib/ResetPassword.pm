999b9ca61f3700a782d680c0c9419a286501aba5
Bruce Woodward <bruce.woodward@gmail.com>
2013-08-26 07:54:24 +1000

    moving function that actually resets the password into the ResetPassword module

--- lib/ResetPassword.pm
+++ lib/ResetPassword.pm
@@ -1,5 +1,28 @@
 
 package ResetPassword;
 
+sub reset_password {
+    my ($expect, $username, $password) = @_;
+    my $number_of_passwords_given = 0;
+
+    $expect->send("passwd $username");
+    $expect->expect( 
+        [ qr/[nN]ew [pP]assword:/m, sub {
+                my ($pty) = @_;
+                $number_of_passwords_given++;
+                $pty->send($password);
+                return exp_continue;
+            }, $expect ],
+
+        [ qr/Re-enter New password:/m, sub {
+                my ($pty) = @_;
+                $number_of_passwords_given++;
+                $pty->send($password);
+                return;
+            }, $expect ]
+    );
+    my $status = $expect->wait_for_shell_prompt;
+    return $status;
+}
 
 1;
